e9b9c1890da22db82e62b95ecdc595643cd7b7ca
Doris Wang (Walile) <fance26@gmail.com>
2024-01-18 11:41:55 +0800

    finish tian gan di zhi

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Chinese_med_five_movement_six_qi
+# Chinese Medicine Five Movement Six Qi 五運六氣 天干地支計算
+
+## 指令功能
+1. tian_gan_di_zhi 天干地支
+利用西元年計算天干地支換算。
+
+    Use AD for exchange Chinese tian gan di zhi. It is a important thing in Canon of medicin, Neiching. (One of Chinese Medicine theory)
+
+2. Five Movement Six Qi 五運六氣排列
+
+    In Chinese medicine, weather is important one of reason effect our healthy. Five Movement Six Qi is a way to estimate the weather in specifict year and help people to think what kind of prescription should be issueing by "聖濟總錄".
--- tian_gan_di_zhi.py
+++ tian_gan_di_zhi.py
@@ -1,20 +1,28 @@
 import datetime
 
-# 定義天干和地支的列表
-tian_gan = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"]
-di_zhi = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
-year_112 = datetime.datetime(2023, 1, 1).year
 
-# 兔年屬於地支中的第4個，因此可以對應到di_zhi列表中的第3個元素
-di_zhi_index = 3
-
-# 根據天干和地支的循環規律，可以計算出112年屬於的天干和地支
-tian_gan_index = (year_112 - 4) % 10  # 4是甲子年的地支序號，10是天干的數量
-tian_gan_year = tian_gan[tian_gan_index]
-di_zhi_year = di_zhi[di_zhi_index]
-
-# 將天干和地支組合起來，得到112年兔年的干支
-gan_zhi_year = tian_gan_year + di_zhi_year
+print("欲計算西元年的天干地支，請按Enter輸入西元年：")
+year = int(input())
+chinese_year = gan_zhi_year(year)
+print('西元{}年是{}年'.format(year, chinese_year))
 
 # 輸出結果
-print("民國112年兔年對應的天干地支是：" + gan_zhi_year)
+print('民國{}年兔年對應的天干地支是： {}'.format(year, gan_zhi_year))
+
+def gan_zhi_year(year):
+
+    # 定義天干和地支的列表
+    tian_gan = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"]
+    di_zhi = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
+
+    # 根據天干和地支的循環規律，可以計算出112年屬於的天干和地支
+    base_index = (year - 4) % 60
+    tian_gan_index = base_index % 10  # 4是甲子年的地支序號，10是天干的數量
+
+    tian_gan_year = tian_gan[tian_gan_index]
+    di_zhi_index = base_index % 12  # 12是地支的數量
+    di_zhi_year = di_zhi[di_zhi_index]
+    # 將天干和地支組合起來，得到干支
+    gan_zhi_year = tian_gan_year + di_zhi_year
+
+    return gan_zhi_year
